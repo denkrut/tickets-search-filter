fdc023345d549b3428b28df35519a22c78a6b340
denkrut <den@digitalho.com>
2023-05-30 15:21:12 +0300

    Save changes before rebase

--- src/index.js
+++ src/index.js
@@ -11,23 +11,11 @@ root.render(
     </React.StrictMode>
   </Provider>
 );
-<<<<<<< HEAD
 //NO CHANGES
-<<<<<<< HEAD
-
 // chnages in app.js main branch
-=======
-=======
 // Code in main branch while Very new issue is created
 //Next Change in main
 //Third Change in main
 //Started working on Very new issue
-<<<<<<< HEAD
->>>>>>> 2f8d0da (rebase from main)
-<<<<<<< HEAD
->>>>>>> a941dbc (rebase from main branch)
-=======
-=======
 //Changes in Very new issue branch after rebase
->>>>>>> ffacb53 (after rebase)
->>>>>>> b9bf5fd (after rebase)
+//Some changes in issue branch before rebase from main
